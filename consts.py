d0f2f5bbab7aa4598720935e715de40fe56756d4
coizioc <36041795+coizioc@users.noreply.github.com>
2019-04-06 22:21:59 -0400

    Added consts.py

---
+++ consts.py
@@ -0,0 +1,24 @@
+# Feel free to adjust any of the parameters here, just be careful if you change these *after* you
+# Run the bot for the first time
+
+# Paths and files
+MESSAGES_DIRECTORY = './messages/'
+MODELS_DIRECTORY = './models/'
+OPTIONS_DIRECTORY = './options/'
+SERVER_JSON_DIRECTORY = './server_json/'
+
+DEATHMATCH_FILE = 'deathmatch.txt'
+NAMES_FILE = 'names.txt'
+SERVERS_FILE = 'servers.txt'
+USER_MODEL_FILE = 'htz_user_model.json'
+LINKS_FILE = 'links.txt'
+CHANNEL_PERMISSIONS_JSON = 'channelperms.json'
+
+# Discord/Bot constants
+DEFAULT_NAME = 'MarkovBot'
+DESCRIPTION = "Bot that creates stuff based off of Markov chains."
+
+MAX_MESSAGE_LENGTH = 1800
+MAX_MARKOV_ATTEMPTS = 10
+MAX_NICKNAME_LENGTH = 30
+MAX_NUM_NAMES = 5
