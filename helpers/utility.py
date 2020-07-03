4f3068d5c2891e0940aff82dc32cd936d0e4177d
coizioc <36041795+coizioc@users.noreply.github.com>
2020-07-02 21:26:05 -0400

    Fix function declarations.

--- helpers/utility.py
+++ helpers/utility.py
@@ -37,14 +37,14 @@ def get_serverid(filename):
 def is_mention(word: str):
     return word.startswith('<@') or word.startswith('<@!')
 
-def get_user_tags(msg: str):
+def get_user_tags(msg: str) -> set:
     user_tags = set()
     for line in msg.splitlines():
         user_tags.update([word for word in line.split() if is_mention(word)])
     return user_tags
 
 
-def remove_mentions(msg, current_guild):
+def remove_mentions(msg, current_guild) -> str:
     """
     Removes mentions from a message.
     :param msg: str
@@ -52,8 +52,6 @@ def remove_mentions(msg, current_guild):
     :return: the message, with mentions removed.
     """
     user_tags = get_user_tags(msg)
-    print(msg)
-    print(user_tags)
     for user_tag in user_tags:
         userid = int(re.sub('\D', '', user_tag))
         username = current_guild.get_member(userid)
@@ -62,7 +60,6 @@ def remove_mentions(msg, current_guild):
             msg = msg.replace(user_tag, '@' + username)
         elif user_tag in msg:
             msg = msg.replace(user_tag, "@UNKNOWN_USER")
-    print(msg)
     return msg
 
 
